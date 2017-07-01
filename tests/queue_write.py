fb2ae46b24a1329d617d8c16294d4c10c82a3581
Heiko Mueller <heiko.muller@gmail.com>
2017-07-01 14:21:34 -0400

    Add queue read/write example

--- tests/queue_write.py
+++ tests/queue_write.py
@@ -6,23 +6,6 @@ import sys
 from scoengine import ModelRunRequest
 
 
-def callback(ch, method, properties, body):
-    """Callback handler for client requests."""
-    # Read model run request (expects Json object)
-    try:
-        request = ModelRunRequest.from_dict(json.loads(body))
-    except Exception as ex:
-        print ex
-        ch.basic_ack(delivery_tag = method.delivery_tag)
-        return
-    # Request identifier for logging purposes
-    req_id = request.experiment_id + ':' + request.run_id
-    # Run request using local worker
-    print 'Start model run [' + req_id + ']'
-    print 'Done [' + req_id + ']'
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-
-
 # Connection parameter
 hostname = 'localhost'
 queue = 'sco'
@@ -80,10 +63,12 @@ con = pika.BlockingConnection(pika.ConnectionParameters(
 ))
 channel = con.channel()
 channel.queue_declare(queue=queue, durable=True)
-# Fair dispatch. Never give a worker more than one message
-channel.basic_qos(prefetch_count=1)
-# Set callback handler to read requests and run the predictive model
-channel.basic_consume(callback, queue=queue)
-# Done. Start by waiting for requests
-print 'Waiting for requests. To exit press CTRL+C'
-channel.start_consuming()
+
+channel.basic_publish(
+    exchange='',
+    routing_key=queue,
+    body=json.dumps(ModelRunRequest('A', 'B', 'C').to_dict()),
+    properties=pika.BasicProperties(
+        delivery_mode = 2, # make message persistent
+    )
+)
