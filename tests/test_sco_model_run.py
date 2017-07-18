f579d5f16fb47423e5989aae4983bdbb5e64a6a8
Heiko Mueller <heiko.muller@gmail.com>
2017-07-18 11:41:23 -0400

    Add tests for cotrical image archive

--- tests/test_sco_model_run.py
+++ tests/test_sco_model_run.py
@@ -65,6 +65,78 @@ class TestSCODataStoreWorker(unittest.TestCase):
             images.identifier,
             {'name':'Test Experiment'}
         )
+        # Create new model run
+        args={
+            'gabor_orientations' : 8,
+            'normalized_pixels_per_degree' : 6.4,
+            'max_eccentricity': 10.0
+        }
+        model_def = self.engine.get_model('benson17')
+        model_run = self.db.experiments_predictions_create(
+            experiment.identifier,
+            model_def.identifier,
+            model_def.parameters,
+            'Test Run',
+            arguments=[{'name' : key, 'value' : args[key]} for key in args]
+        )
+        SCODataStoreWorker(self.db, self.engine, ENV_DIR).run(
+            ModelRunRequest(
+                model_run.identifier,
+                experiment.identifier,
+                'ok to have a dummy Url here'
+            )
+        )
+        # Ensure that model run is in SUCCESS state
+        model_run = self.db.experiments_predictions_get(
+            experiment.identifier,
+            model_run.identifier
+        )
+        self.assertTrue(model_run.state.is_success)
+        # Make sure that the attachments are correct. We expect one attachment
+        # containing the list of images.\# Count the number of lines in the
+        # images.txt file. This should be the same as the number of images in
+        # the image group
+        self.assertTrue('images.txt' in model_run.attachments)
+        line_count = 0
+        image_list_file = self.db.experiments_predictions_attachments_download(
+            experiment.identifier,
+            model_run.identifier,
+            'images.txt'
+        ).file
+        with open(image_list_file, 'r') as f:
+            for line in f:
+                line_count += 1
+        self.assertEqual(line_count, len(images.images))
+        # All other attachments that are defined in the model should exist
+        cort_images_file = None
+        for attmnt in model_def.outputs.attachments:
+            filename = self.db.experiments_predictions_attachments_download(
+                experiment.identifier,
+                model_run.identifier,
+                attmnt.filename
+            ).file
+            self.assertTrue(os.path.isfile(filename))
+            if attmnt.filename == 'cortical-images.tar.gz':
+                cort_images_file = filename
+        #self.assertNotIsNone(cort_images_file)
+        if not is None cort_images_file:
+            self.assertTrue(validate_cortical_images_file(cort_images_file, 10, True))
+
+    def test_successful_model_run_with_funcdata(self):
+        """Test local SCO worker. Create experiment from subject and image group
+        files. Run experiment to validate code."""
+        # Create subject, image group and experiment
+        subject = self.db.subjects_create(self.SUBJECT_FILE)
+        images = self.db.images_create(self.IMAGES_ARCHIVE)
+        self.db.image_groups_update_options(
+            images.identifier,
+            [Attribute('pixels_per_degree', 6.4)]
+        )
+        experiment = self.db.experiments_create(
+            subject.identifier,
+            images.identifier,
+            {'name':'Test Experiment'}
+        )
         self.db.experiments_fmri_create(experiment.identifier, self.FUNC_DATA)
         # Create new model run
         args={
@@ -109,6 +181,7 @@ class TestSCODataStoreWorker(unittest.TestCase):
                 line_count += 1
         self.assertEqual(line_count, len(images.images))
         # All other attachments that are defined in the model should exist
+        cort_images_file = None
         for attmnt in model_def.outputs.attachments:
             filename = self.db.experiments_predictions_attachments_download(
                 experiment.identifier,
@@ -116,7 +189,19 @@ class TestSCODataStoreWorker(unittest.TestCase):
                 attmnt.filename
             ).file
             self.assertTrue(os.path.isfile(filename))
+            if attmnt.filename == 'cortical-images.tar.gz':
+                cort_images_file = filename
+        #self.assertNotIsNone(cort_images_file)
+        if not is None cort_images_file:
+            self.assertTrue(validate_cortical_images_file(cort_images_file, 10, True))
 
+
+def validate_cortical_images_file(filename, image_count, has_func_data):
+    # Change the body of this function. Unzip and unpack the tar file into a
+    # temporal directory. Count the number of images files that are in the
+    # unpacked directory and return True if the number equals the image count
+    # times 3 (if has_func_data is False) or 6 (is has_func_data is True).
+    return False
 
 if __name__ == '__main__':
     unittest.main()
